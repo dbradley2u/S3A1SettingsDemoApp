6b80d0c3e7d84e1332cf55f185ec18389d7d9070
Denise Bradley <porenn4u@outlook.com>
2016-05-31 20:33:07 -0400

    S3A1 Settings Demo Initial Commit.

---
+++ S3A1SettingsDemoApp/Views/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace S3A1SettingsDemoApp.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
